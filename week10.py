02fb93a1f03bbeb8e21bd0c0d11a8aaf3e959048
jihoo12 <134384139+jihoo12@users.noreply.github.com>
2023-08-22 19:40:51 +0900

    Add files via upload

--- week10.py
+++ week10.py
@@ -20,9 +20,77 @@
 # num = [int(i) for i in input().split()]
 # print(sum(num))
 ### 중첩 for문
-num = int(input())
-for i in range(1,num+1):
-    for j in range(num,i,-1):
-        print(' ', end='')
-    print('*' * i)    
-###피라미드출력    
+# num = int(input())
+# for i in range(1,num+1):
+#     for j in range(num,i,-1):
+#         print(' ', end='')
+#     print('*' * i)    
+# 피라미드출력    
+# num = int(input())
+# for i in range(1,num+1):
+    # for j in range(num,i,-1):
+        # print(' ', end='')     
+    # print('*' * ((i*2)-1))
+### 중간평가 
+## 1.
+# stri = str(input("str:"))
+# num = int(input("int:"))
+# print(f"{stri}{num}")
+
+## 2.
+# s = "과천코딩2022"
+# print(s[2:6])
+
+## 3.
+# stri = str(input("str:"))
+# num = int(input("int:"))
+# print(f"어제 {stri}를 {num}개 먹었습니다.")
+
+## 4.
+# num = int(input("int:"))
+# num1 = int(input("int:"))
+# print(num+num1)
+# print(num-num1)
+# print(num*num1)
+# print(num/num1)
+
+## 5.
+# num = int(input("int:"))
+# if num % 2 == 0:
+#     print("짝수입니다")
+# else:
+#     print("홀수입니다")
+
+## 6.
+# num = int(input("int:"))
+# for i in range(1,num+1):
+#     if num % i == 0:
+#         print(i,end=" ")
+
+## 7.
+# num1 = []
+# num = int(input("int:"))
+# for i in range(1,num+1):
+#      if num % i == 0:
+#          num1.append(i)
+# if len(num1) == 2:
+#     print("true")  
+# else:
+#     print("false")     
+
+## 8.
+# num = int(input("int:"))
+# for i in range(1,10):
+#     print(f"{num}*{i}={num*i}")
+
+## 9.
+# num1 = []
+# num = int(input("int:"))
+# for i in range(1,num+1):
+#     num1.append(i)
+# print(sum(num1)) 
+
+## 10.
+num = int(input("int:"))
+for i in range(1, num+1):
+    print(i)
